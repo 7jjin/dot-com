32e17d4c0a8cf3994406fdc9e0f9d762732db3ad
jsj487 <121417103+jsj487@users.noreply.github.com>
2022-12-28 02:24:24 +0900

    Help

--- JoSeungJun/Sign_up.js
+++ JoSeungJun/Sign_up.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // document.addEventListener('keydown', function(event) { //엔터키가 적용이 안되게 막는 이벤트
+    //     if (event.keyCode === 13) {
+    //       event.preventDefault();
+    //     };
+    //   }, true);
+    function IDsubmit(e){
+      e.addEventListener("submit",function(){
+        let userInfo = this.children[0].value;
+        localStorage.setItem('ID',userInfo);
+      });
+    }
+    function PASSsubmit(e){
+      e.addEventListener("submit",function(){
+        let userInfo = this.children[0].value;
+        localStorage.setItem('PASS',userInfo);
+      });
+    }
     
     const open = () => { //회원가입 모달창 열기
         document.querySelector(".modal").classList.remove("hidden");
@@ -8,8 +25,20 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".modal").classList.add("hidden");
     }
     
+    const ID = document.querySelector("#Id");
+    const PASS = document.querySelector("#Pass");
+    const ConfirmPASS = document.querySelector("#ConfirmPass");
+    const NAME = document.querySelector("#Name");
+    const DATE = document.querySelector("#DateBirth");
+    const PHONE = document.querySelector("#Phone");
+
+    document.querySelector(".joinBtn").addEventListener("click",function(){
+      IDsubmit(ID);
+    });
+
+    IDsubmit(ID);
+
     document.querySelector(".SignUpBtn").addEventListener("click", open); //회원가입 버튼 눌렀을 때 이벤트
     document.querySelector(".joinBtn").addEventListener("click", close); //모달창에서 가입하기 버튼 눌렀을 때 이벤트
     document.querySelector(".bg").addEventListener("click", close); //모달창 배경을 눌렀을 때 닫히는 이벤트
-    
 });
