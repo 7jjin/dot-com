a7d4287428d0bdff60c7da6fcb699fdda84f177a
jsj487 <121417103+jsj487@users.noreply.github.com>
2023-01-08 02:10:40 +0900

    수정

--- JoSeungJun/FindInfo/FindInfo.js
+++ JoSeungJun/FindInfo/FindInfo.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     const RadioId = document.querySelector("#Id");
     const RadioPass = document.querySelector("#Pass");
-    const IdSub = document.querySelector(".IdBtn");
-    const PassSub = document.querySelector(".PassBtn");
-    const FindId = document.querySelector(".FindId");
-    const FindPass = document.querySelector(".FindPass");
+    const IdSub = document.querySelector(".FindId");
+    const PassSub = document.querySelector(".FindPass");
 
-    function Check(){
+    function Check(event){
         const ID = RadioId.checked;
         const PASS = RadioPass.checked;
 
@@ -20,7 +18,32 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    function IdFind(event){
+        const Id = IdSub
+
+        event.preventDefault();
+
+        if(Id){
+            document.querySelector(".IdDiv").style.display = 'none'
+            document.querySelector(".IdBtn").style.display = 'none'
+            document.querySelector(".IdDiv2").style.display = 'block'
+        }
+    }
+    function PassFind(event){
+        const Pass = PassSub
+
+        event.preventDefault();
+
+        if(Pass){
+            document.querySelector(".PassDiv").style.display = 'none'
+            document.querySelector(".PassBtn").style.display = 'none'
+            document.querySelector(".PassDiv2").style.display = 'block'
+        }
+    }
+
     RadioId.addEventListener("click", Check);
     RadioPass.addEventListener("click", Check);
+    IdSub.addEventListener("submit", IdFind);
+    PassSub.addEventListener("submit", PassFind);
 });
 
