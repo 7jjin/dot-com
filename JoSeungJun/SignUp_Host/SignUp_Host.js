2bc7e9476038c4f0a68592d458a4b34f2e61712e
jsj487 <121417103+jsj487@users.noreply.github.com>
2023-01-15 00:53:41 +0900

    주인 회원가입

--- JoSeungJun/SignUp_Host/SignUp_Host.js
+++ JoSeungJun/SignUp_Host/SignUp_Host.js
@@ -3,14 +3,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const Join = document.querySelector(".join");
   const PwInput = document.querySelector("#PassInput");
   const ConfirmPassInput = document.querySelector("#ConfirmPassInput");
+  const PassInput = document.querySelector("#PassInput");
   const EmailInput = document.querySelector(".EmailInput");
   const errorbox = document.querySelector("#email_errorbox");
+  const Good = document.querySelector("#GoodPass");
+  const Bad = document.querySelector("#BadPass");
+  const JoinBtn = document.querySelector(".joinBtn");
+  const NotBtn = document.querySelector(".NotBtn");
 
-  function emailCheck() {
+
+  function Check() {
     var Email = EmailInput.value;
+    var Pass = PassInput.value;
+    var Confirm = ConfirmPassInput.value;
     var re = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
 
-    if (re.test(Email) === false) {
+    if (re.test(Email) === false) { //이메일 양식 확인
       errorbox.style.visibility = "visible";
     } else {
       errorbox.style.visibility = "hidden";
@@ -18,31 +26,43 @@ document.addEventListener("DOMContentLoaded", function () {
     if (Email === '') {
       errorbox.style.visibility = "hidden";
     }
-  }
 
-  function PassCheck() {
-    if (document.getElementById('PassInput').value != '' && document.getElementById('ConfirmPassInput').value != '') {
-      if (document.getElementById('PassInput').value == document.getElementById('ConfirmPassInput').value) {
-        document.querySelector("#GoodPass").style.display = "block";
-        document.querySelector("#BadPass").style.display = "none";
+    if (Pass != '' && Confirm != '') { //비밀번호확인
+      if (Pass == Confirm) {
+        Good.style.display = "block";
+        Bad.style.display = "none";
       }
       else {
-        document.querySelector("#BadPass").style.display = "block";
-        document.querySelector("#GoodPass").style.display = "none";
+        Good.style.display = "none";
+        Bad.style.display = "block";    
+      }
+    }
+
+    if (Pass != '' && Confirm != '') { //비밀번호가 일치하고 이메일 양식이 맞으면 회원가입 버튼 활성화
+      if ((Pass == Confirm) && (re.test(Email) === true)) {
+        JoinBtn.style.display = "block"
+        NotBtn.style.display = "none"
+      }
+      else{
+        JoinBtn.style.display = "none"
+        NotBtn.style.display = "block"
       }
     }
   }
 
-  function validatePassword() {
+  function validatePassword(){
     event.preventDefault();
 
-    if (PwInput.value != ConfirmPassInput.value) {
-      ConfirmPassInput.setCustomValidity("Passwords Don't Match");
+    if(PwInput.value != ConfirmPassInput.value) {
+      alert("비밀번호가 일치하지 않습니다.");
+    } else {
+      ConfirmPassInput.setCustomValidity(''); // 오류가 없으면 메시지를 빈 문자열로 설정해야한다. 오류 메시지가 비어 있지 않은 한 양식은 유효성 검사를 통과하지 않고 제출되지 않는다.
     }
   }
 
-  ConfirmPassInput.addEventListener('blur', PassCheck);  //email의 형식이 틀릴경우 오류 문자 생성
-  EmailInput.addEventListener('blur', emailCheck);  //email의 형식이 틀릴경우 오류 문자 생성
+  ConfirmPassInput.addEventListener('blur', Check);  //email의 형식이 틀릴경우 오류 문자 생성
+  PassInput.addEventListener('blur', Check);  //email의 형식이 틀릴경우 오류 문자 생성
+  EmailInput.addEventListener('blur', Check);  //email의 형식이 틀릴경우 오류 문자 생성
   Join.addEventListener("submit", validatePassword);   // submit했을때 해당 함수 실행   
 });
 
