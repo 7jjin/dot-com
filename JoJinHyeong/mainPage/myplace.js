cc1c7ffbd1bdb1e09bd9217c20f93c78b00cdc8f
7jjin <wlsgud6305@naver.com>
2023-01-28 16:08:07 +0900

    현재 위치를 행정구역으로 나타내기(실패)

--- JoJinHyeong/mainPage/myplace.js
+++ JoJinHyeong/mainPage/myplace.js
@@ -1,62 +1,45 @@
-navigator.geolocation.getCurrentPosition(getSuccess, getError);
-
-function getSuccess(position){
-    const lat = position.coords.latitude;
-    const lng = position.coords.logitude;
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            let lon = position.coords.latitude; //위도
+            let lat = position.coords.longitude; //경도
+            toAddress(lon,lat);
+        }, function(error) {
+          console.error(error);
+        }, {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity
+        });
+    }
 }
-function getError(){
-    alert("Error");
+
+
+function toAddress(lon,lat){
+    console.log(`lon: ${lon}, lat: ${lat}`);
+    $.ajax({
+        url : `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${lat}&y=${lon}&input_coord=WGS84`,
+        type : 'GET',
+        headers : {
+          Authorization : 'kakaoAK {4c52e79e01d745f8fccd4b35c17788e6}'
+        },
+        success : function(result) {
+            let totatlCount = result.meta.total_count; //총 문서 수 
+             if (totatlCount > 0) {
+                if (result.documents[0].road_address === null) {
+                    addressName = result.documents[0].address.region_1depth_name; //지역(시) 이름 
+                } else {
+                    addressName = result.documents[0].road_address.region_1depth_name;
+                }
+            }
+            addr = addressName;
+        },
+        error : function(e) {
+          console.log(e);
+        }
+    });
 }
 
-
-
-
-
-
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
-mapOption = { 
-    center: new kakao.maps.LatLng(lat, lng), // 지도의 중심좌표
-    level: 3 // 지도의 확대 레벨
-};
-
-var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
-
-// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-var mapTypeControl = new kakao.maps.MapTypeControl();
-
-// 지도 타입 컨트롤을 지도에 표시합니다
-map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-function getInfo() {
-// 지도의 현재 중심좌표를 얻어옵니다 
-var center = map.getCenter(); 
-
-// 지도의 현재 레벨을 얻어옵니다
-var level = map.getLevel();
-
-// 지도타입을 얻어옵니다
-var mapTypeId = map.getMapTypeId(); 
-
-// 지도의 현재 영역을 얻어옵니다 
-var bounds = map.getBounds();
-
-// 영역의 남서쪽 좌표를 얻어옵니다 
-var swLatLng = bounds.getSouthWest(); 
-
-// 영역의 북동쪽 좌표를 얻어옵니다 
-var neLatLng = bounds.getNorthEast(); 
-
-// 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
-var boundsStr = bounds.toString();
-
-
-var message = '지도 중심좌표는 위도 ' + center.getLat() + ', <br>';
-message += '경도 ' + center.getLng() + ' 이고 <br>';
-message += '지도 레벨은 ' + level + ' 입니다 <br> <br>';
-message += '지도 타입은 ' + mapTypeId + ' 이고 <br> ';
-message += '지도의 남서쪽 좌표는 ' + swLatLng.getLat() + ', ' + swLatLng.getLng() + ' 이고 <br>';
-message += '북동쪽 좌표는 ' + neLatLng.getLat() + ', ' + neLatLng.getLng() + ' 입니다';
-
-// 개발자도구를 통해 직접 message 내용을 확인해 보세요.
-// ex) console.log(message);
-}
+$("#btnTest").click(function() {
+	getLocation();
+})
