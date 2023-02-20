69f86782a5a768c29ddef5a7bbe90aa338ed95dd
7jjin <wlsgud6305@naver.com>
2023-02-20 22:28:39 +0900

    menu plus

--- JoJinHyeong/Store_info/store.js
+++ JoJinHyeong/Store_info/store.js
@@ -51,3 +51,26 @@ function change_btn(e) {
 }
 // 가로 스크롤 - 버튼 누르면 해당 버튼의 색이 변하고 다른 버튼을 누르면 해제되는 함수
 // ----------------------------------------------------------------------------
+const number = document.querySelectorAll(".number span");
+const menu = document.querySelectorAll(".SubItem");
+number.forEach((item,index)=>{
+  item.addEventListener("click",function(e){
+    bar.style.borderRadius = "20px";
+    bar.style.justifyContent= 'flex-Start';
+    bar.style.marginLeft = ""
+    bar.style.width = "170px";
+    bar.style.right = "-35%";
+    bar.style.flexDirection = "column";
+    bar.style.height = "none"
+    bar.style.alignItems = "normal";
+    bar.innerHTML = `<div class="member">
+    <p>인원: ${e.target.innerText}</p>
+    </div>`;
+  });
+});
+menu.forEach((item,index)=>{
+  item.addEventListener("click",function(e){
+    bar.innerHTML += `
+    <p>메뉴${index+1}: ${e.target.firstElementChild.firstElementChild.innerText}</p>`;
+  })
+})
