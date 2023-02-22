9bf19faaabd83c46173004fcdc59d2f05d6baedd
7jjin <wlsgud6305@naver.com>
2023-02-22 14:40:09 +0900

    수량 변경하면 가격 증가및 감소
    해당 메뉴의 가격이 변하는 것이 아닌 맨 위의 메뉴만 바뀜

--- JoJinHyeong/Store_info/store.js
+++ JoJinHyeong/Store_info/store.js
@@ -71,17 +71,53 @@ number.forEach((item,index)=>{
 // ------ 인원
 menu.forEach((item)=>{
   item.addEventListener("click",function(e){
+    //console.log(e.target.firstElementChild.children[1].innerText)
     bar.innerHTML += `
     <div class="menuBox">
-      <div class="menuName">${e.target.firstElementChild.firstElementChild.innerText}</div>
-      <div class="menuCost">
-        <div class="menuCost_left>
-          <button class="delete"></button>
-          <div class="cost">19000</div>
-        </div>
-        <div class="menuCost_right>
-          <div
-      </div>
-    </div>`;
+          <div class="menuName">${e.target.firstElementChild.firstElementChild.innerText}</div>
+          <div class="menuCost">
+            <div class="menuCost_left">
+              <button class="delete" onclick="del()">X</button>
+              <div class="cost" value="${e.target.firstElementChild.children[1].innerText}">${e.target.firstElementChild.children[1].innerText}원</div>
+            </div>
+            <div class="menuCost_right">
+              <button class="minus" onclick="minus()">-</button>
+              <div class="su">1</div>
+              <button class="plus" onclick="plus()">+</button>
+            </div>
+          </div>`;
   },{once:true})  // once:true 한번만 클릭하게 하기
 })
+
+
+// 메뉴 추가하기
+// -------------------------------------------------------------------------------------------------------
+function plus(e){
+  const result = document.querySelector(".su");
+  const cost = document.querySelector(".cost");
+  const realcost = cost.getAttribute("value");    // cost의 value값 고정 (19000)
+  let total = parseInt(cost.innerText);   //19000원 (계속 바뀌는 값)
+  let number = parseInt(result.innerText);  //수량 (계속 바뀌는 값)
+  console.log(cost.innerText)
+  number = parseInt(number) +1;
+  total = parseInt(total)+parseInt(realcost);
+  result.innerText = number;    // 클릭 할때마다 +1
+  cost.innerText = `${total}원`;      // 클릭 할떄마다 +19000
+}
+function minus(e){
+  const result = document.querySelector(".su");
+  const cost = document.querySelector(".cost");
+  const realcost = cost.getAttribute("value");
+  let number = result.innerText;
+  let total = cost.innerText;
+  if(number>1){     // 수량이 1 이상일때마 작동
+    number = parseInt(number) -1;
+    total = parseInt(total)-parseInt(realcost);
+  }
+  result.innerText = number;
+  cost.innerText = `${total}원`;
+}
+function del(e){
+  const menuBox = document.querySelector(".menuBox");
+  menuBox.remove();
+}
