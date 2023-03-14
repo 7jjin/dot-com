64841aafacae09264046735280ebf75a950bddfc
7jjin <wlsgud6305@naver.com>
2023-03-14 21:43:52 +0900

    change

--- JoJinHyeong/Store_info/store.js
+++ JoJinHyeong/Store_info/store.js
@@ -1,3 +1,5 @@
+// 인원수 가로 스크롤
+// ---------------------------------------------------------------------------
 const slider = document.querySelector('.barlist');
 let isMouseDown = false;
 let startX, scrollLeft;
@@ -24,7 +26,7 @@ slider.addEventListener('mousemove', (e) => {
   slider.scrollLeft = scrollLeft - walk;
 });
 
-// 인원수 가로 스크롤
+// 플로팅 버튼
 // ---------------------------------------------------------------------------
 
 const bar = document.querySelector(".remote_Bar");
@@ -36,9 +38,9 @@ window.addEventListener('scroll', function(){
       bar.style.top = '100px';
     }
 });
-// 플로팅 버튼
-// ---------------------------------------------------------------------------
 
+// 가로 스크롤 - 버튼 누르면 해당 버튼의 색이 변하고 다른 버튼을 누르면 해제되는 함수
+// ----------------------------------------------------------------------------
 function change_btn(e) {
   var btns = document.querySelectorAll(".number span");
   btns.forEach(function (btn) {
@@ -49,8 +51,9 @@ function change_btn(e) {
     }
   });
 }
-// 가로 스크롤 - 버튼 누르면 해당 버튼의 색이 변하고 다른 버튼을 누르면 해제되는 함수
-// ----------------------------------------------------------------------------
+
+// 인원을 고르면 플로팅 바의 모양이 바뀜
+//--------------------------------------------------------------------------------
 const number = document.querySelectorAll(".number span");
 const menu = document.querySelectorAll(".SubItem");
 let cart =[];   // 장바구니 배열
@@ -67,15 +70,19 @@ number.forEach((item,index)=>{
     bar.innerHTML = `<div class="member">
     <p>인원: ${e.target.innerText}</p>
     </div>
-    <div class="bottomline"></div>`;
+    <div class="bottomline"></div>
+    <div class="menuBoxOuter"></div>
+    <div class="costHap"></div>`;
   });
 });
-// ------ 인원추가
+// ------ 메뉴추가
 menu.forEach((item,index)=>{
   item.addEventListener("click",function(e){
+    
     const line = document.querySelector(".bottomline");
-    line.style.visibility = "visible";
-    bar.innerHTML += `
+    const menuBoxOuter = document.querySelector(".menuBoxOuter");     // 총합DIV을 맨 아래 넣기 위해 각각의 메뉴태그들을 감싸는 부모 태그를 만들었다.
+    
+    menuBoxOuter.innerHTML += `
     <div class="menuBox" value="${index}">
           <div class="menuName">${e.target.firstElementChild.firstElementChild.innerText}</div>
           <div class="menuCost">
@@ -96,9 +103,19 @@ menu.forEach((item,index)=>{
       price: `${e.target.firstElementChild.children[1].innerText}`
     };
     // 새로 추가될때마다 총합 태그는 가장 아래 위치됨
-    const newElement = document.createElement("div")
-      bar.appendChild(newElement);
     cart.push(product);
+    hap();
+    line.style.visibility = "visible";
+    const costhap = document.querySelector(".costHap");
+    costhap.style.display="flex";       // 합계 DIV 보이게 하기
+    costhap.innerHTML = `
+    <div class="hapDiv">
+      <div class="hapname">합계: </div>
+      <div class="hap">아직몰라</div>
+      <div>원</div>
+    </div>
+    `
+    
   },{once:true})  // once:true 한번만 클릭하게 하기
   
 })
@@ -106,7 +123,7 @@ menu.forEach((item,index)=>{
     
 
 
-// 메뉴 추가하기
+// 인원 추가하기
 // -------------------------------------------------------------------------------------------------------
 
 function plus(el){
@@ -121,12 +138,14 @@ function plus(el){
   result.innerText = number;
   cost.innerText = `${total}`;
   cart[uni].price = total;          // 변화된 가격을 각각의 요소에 맞는 객체를 찾아서 바꿔준다.
-  
+  hap();
+
 }
 
 
 
 function minus(el){
+  let uni = el.parentNode.parentNode.parentNode.getAttribute("value");
   const result = el.parentNode.parentNode.childNodes[3].childNodes[3];  //총 인원태그
   let cost = el.parentNode.parentNode.firstElementChild.childNodes[3];  //총 가격태그
   let total = el.parentNode.parentNode.firstElementChild.childNodes[3].innerText;  //총 가격
@@ -138,27 +157,37 @@ function minus(el){
   }
   result.innerText = number;
   cost.innerText = `${total}`;
+  cart[uni].price = total;
+  hap();
+  console.log(cart)
+
 }
 
 function del(el){         //박스 삭제
+  let uni = el.parentNode.parentNode.parentNode.getAttribute("value");
   const menuBox = document.querySelector(".menuBox");
   menuBox.remove();
+  cart.splice(uni,1);
+  hap();
+  console.log(cart)
 }
 
 // 총 가격 구하는 함수
 //---------------------------------------------------------------------------------------------
 
-console.log(cart);
-console.log(cart.price);
+function hap(){
+    var hapcost = 0;
+    for(let i=0;i<cart.length;i++){
+      hapcost +=Number(cart[i].price);
+    }
+    console.log(hapcost)
+}
+
 
 
 
 // 버튼 누르면 해당 이벤트 적용
 // --------------------------------------------------------------------------------------------
-
-
-
-
 const Filter = document.querySelector('.filter');
 const Blank = document.querySelector('.blank');
 const HomeTab = document.querySelector('.homeTab');
