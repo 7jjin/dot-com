2c8729ddb7657a3b1a747a5389fc9c663b35d172
jsj487 <121417103+jsj487@users.noreply.github.com>
2023-03-21 09:58:17 +0900

    jsj

--- JoJinHyeong/Store_info/store.js
+++ JoJinHyeong/Store_info/store.js
@@ -3,28 +3,28 @@ const Photo_slider = document.querySelector('.PhotoList');
 let isMouseDown = false;
 let startX, scrollLeft;
 
-function slider(Element){
-Element.addEventListener('mousedown', (e) => {
-  isMouseDown = true;
-  scrollLeft = Element.scrollLeft;
-  startX = e.pageX - Element.offsetLeft;
-});
+function slider(Element) {
+  Element.addEventListener('mousedown', (e) => {
+    isMouseDown = true;
+    scrollLeft = Element.scrollLeft;
+    startX = e.pageX - Element.offsetLeft;
+  });
 
-Element.addEventListener('mouseleave', () => {
-  isMouseDown = false;
-});
+  Element.addEventListener('mouseleave', () => {
+    isMouseDown = false;
+  });
 
-Element.addEventListener('mouseup', () => {
-  isMouseDown = false;
-});
+  Element.addEventListener('mouseup', () => {
+    isMouseDown = false;
+  });
 
-Element.addEventListener('mousemove', (e) => {
-  if (!isMouseDown) return;
-  e.preventDefault();
-  const x = e.pageX - Element.offsetLeft;
-  const walk = (x - startX) * 1;
-  Element.scrollLeft = scrollLeft - walk;
-});
+  Element.addEventListener('mousemove', (e) => {
+    if (!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - Element.offsetLeft;
+    const walk = (x - startX) * 1;
+    Element.scrollLeft = scrollLeft - walk;
+  });
 }
 
 // 인원수 가로 스크롤
@@ -32,12 +32,12 @@ Element.addEventListener('mousemove', (e) => {
 
 const bar = document.querySelector(".remote_Bar");
 
-window.addEventListener('scroll', function(){
-    bar.style.top = `${window.scrollY}px`;
-    bar.style.transition = "all 0.7s ease-out";
-    if(window.scrollY===0){
-      bar.style.top = '100px';
-    }
+window.addEventListener('scroll', function () {
+  bar.style.top = `${window.scrollY}px`;
+  bar.style.transition = "all 0.7s ease-out";
+  if (window.scrollY === 0) {
+    bar.style.top = '100px';
+  }
 });
 // 플로팅 버튼
 // ---------------------------------------------------------------------------
@@ -57,10 +57,10 @@ function change_btn(e) {
 const number = document.querySelectorAll(".number span");
 const menu = document.querySelectorAll(".SubItem");
 
-number.forEach((item,index)=>{
-  item.addEventListener("click",function(e){
+number.forEach((item, index) => {
+  item.addEventListener("click", function (e) {
     bar.style.borderRadius = "20px";
-    bar.style.justifyContent= 'flex-Start';
+    bar.style.justifyContent = 'flex-Start';
     bar.style.marginLeft = ""
     bar.style.width = "220px";
     bar.style.right = "-35%";
@@ -74,8 +74,8 @@ number.forEach((item,index)=>{
   });
 });
 // ------ 인원추가
-menu.forEach((item)=>{
-  item.addEventListener("click",function(e){
+menu.forEach((item) => {
+  item.addEventListener("click", function (e) {
     const line = document.querySelector(".bottomline");
     line.style.visibility = "visible";
     bar.innerHTML += `
@@ -93,43 +93,43 @@ menu.forEach((item)=>{
               <button class="plus" onclick="plus(this)">+</button>
             </div>
           </div>`;
-    
-  },{once:true})  // once:true 한번만 클릭하게 하기
+
+  }, { once: true })  // once:true 한번만 클릭하게 하기
 })
 
-    
+
 
 
 // 메뉴 추가하기
 // -------------------------------------------------------------------------------------------------------
 
-function plus(el){
+function plus(el) {
   const result = el.parentNode.parentNode.childNodes[3].childNodes[3];  //총 인원태그
   let cost = el.parentNode.parentNode.firstElementChild.childNodes[3];  //총 가격태그
   let total = el.parentNode.parentNode.firstElementChild.childNodes[3].innerText;  //총 가격
   let number = el.parentNode.parentNode.childNodes[3].childNodes[3].innerText; //총 수
   const realcost = el.parentNode.parentNode.firstElementChild.childNodes[3].getAttribute("value"); //메뉴 하나당 가격(불변)
-  number = parseInt(number)+1;
-  total = parseInt(total)+parseInt(realcost);
+  number = parseInt(number) + 1;
+  total = parseInt(total) + parseInt(realcost);
   result.innerText = number;
   cost.innerText = `${total}`;
 }
 
-function minus(el){
+function minus(el) {
   const result = el.parentNode.parentNode.childNodes[3].childNodes[3];  //총 인원태그
   let cost = el.parentNode.parentNode.firstElementChild.childNodes[3];  //총 가격태그
   let total = el.parentNode.parentNode.firstElementChild.childNodes[3].innerText;  //총 가격
   let number = el.parentNode.parentNode.childNodes[3].childNodes[3].innerText; //총 수
   const realcost = el.parentNode.parentNode.firstElementChild.childNodes[3].getAttribute("value"); //메뉴 하나당 가격(불변)
-  if(number>1){     // 수량이 1 이상일때마 작동
-    number = parseInt(number) -1;
-    total = parseInt(total)-parseInt(realcost);
+  if (number > 1) {     // 수량이 1 이상일때마 작동
+    number = parseInt(number) - 1;
+    total = parseInt(total) - parseInt(realcost);
   }
   result.innerText = number;
   cost.innerText = `${total}`;
 }
 
-function del(el){         //박스 삭제
+function del(el) {         //박스 삭제
   const menuBox = document.querySelector(".menuBox");
   menuBox.remove();
 }
@@ -152,49 +152,91 @@ const Photo = document.querySelector('.storePhoto');
 const Photo_Top = window.pageYOffset + Photo.getBoundingClientRect().top;
 const Review = document.querySelector('.storeReview');
 const Review_Top = window.pageYOffset + Review.getBoundingClientRect().top;
+const ReviewMore = document.querySelector('.ReviewMore');
+const ReviewMore1 = document.querySelector('.ReviewMore1');
+const ReviewBox = document.querySelector('.ReviewBox');
+const PhotoMore = document.querySelector('.PhotoMore');
+const PhotoBox = document.querySelector('.PhotoBox');
+const Right = document.querySelector('.Right');
+const Left = document.querySelector('.Left');
+const PhotoReduce = document.querySelector('.PhotoReduce');
+const ReviewReduce = document.querySelector('.ReviewReduce');
 
-function Top(){
-
-  if(window.scrollY > 625){
+function Top() {
+  if (window.scrollY > 625) {
     Filter.style.position = "fixed";
     Filter.style.top = "0";
   }
-  else if(window.scrollY < 625){
+  else if (window.scrollY < 625) {
     Filter.style.position = "";
     Filter.style.top = "";
   }
 }
 
-function Move(element){
-  window.scroll({top : element - 60, behavior: 'smooth'});
+function Move(element) {
+  window.scroll({ top: element - 60, behavior: 'smooth' });
+}
+function Expansion(Element){
+  var addHeight = 1000;
+  var currentHeight = Element.clientHeight;
+  var newHeight = currentHeight + addHeight;
+
+  Element.style.height = newHeight + "px";
 }
 
-function ArrowHide(element){
-  
-}
-
-HomeTab.addEventListener('click', function(){
+HomeTab.addEventListener('click', function () {
   Move(Home_Top);
 });
-MenuTab.addEventListener('click', function(){
+MenuTab.addEventListener('click', function () {
   Move(Menu_Top);
 });
-PhotoTab.addEventListener('click', function(){
+PhotoTab.addEventListener('click', function () {
   Move(Photo_Top);
 });
-ReviewTab.addEventListener('click', function(){
+ReviewTab.addEventListener('click', function () {
   Move(Review_Top);
 });
 
-Bar_slider.addEventListener('mousemove', function(){
+Bar_slider.addEventListener('mousemove', function () {
   slider(Bar_slider);
 });
-Photo_slider.addEventListener('mousemove', function(){
+Photo_slider.addEventListener('mousemove', function () {
   slider(Photo_slider);
 });
 
-window.addEventListener('scroll', () => { 
+window.addEventListener('scroll', () => {
   Top();
 });
+
+PhotoMore.addEventListener('click', () => {
+  PhotoBox.style.height = "1000px";
+  PhotoMore.style.display = "none";
+  PhotoReduce.style.display = "block";
+  Right.style.visibility = "hidden";
+  Left.style.visibility = "hidden";
+});
+ReviewMore.addEventListener('click', () => {
+  PhotoBox.style.height = "1000px";
+  ReviewMore.style.display = "none";
+  ReviewReduce.style.display = "block";
+});
+
+PhotoReduce.addEventListener('click', () => {
+  PhotoBox.style.height = "200px";
+  PhotoMore.style.display = "block";
+  PhotoReduce.style.display = "none";
+});
+ReviewReduce.addEventListener('click', () => {
+  ReviewBox.style.height = "600px";
+  ReviewMore.style.display = "block";
+  ReviewReduce.style.display = "none";
+});
+
+ReviewMore1.addEventListener('click', () => {
+  Expansion(ReviewBox);
+  ReviewMore.style.display = "none";
+  ReviewReduce.style.display = "block";
+});
+
 
 
---
+++ JoSeungJun/Owner_Main/Owner_Main.js
@@ -0,0 +1,1 @@
+const First = document.querySelector('.first')
