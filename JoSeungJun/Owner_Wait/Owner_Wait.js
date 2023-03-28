d679a2b791d79e12b32959568d741b909946822a
jsj487 <121417103+jsj487@users.noreply.github.com>
2023-03-28 18:43:09 +0900

    대기열 표

---
+++ JoSeungJun/Owner_Wait/Owner_Wait.js
@@ -0,0 +1,63 @@
+const X = document.querySelector(".X-image");
+const details = document.querySelector(".details");
+const TRtables = document.querySelectorAll(".TRtable");
+const NumberTDS = document.querySelectorAll(".NumberTD")
+const NameTDS = document.querySelectorAll(".NameTD");
+const MenTDS = document.querySelectorAll(".MenTD");
+const VisitTDS = document.querySelectorAll(".VisitTD");
+const PhoneTDS = document.querySelectorAll(".PhoneTD");
+const WaiterName = document.querySelector(".WaiterName");
+const NumText = document.querySelector(".NumText");
+const CountNumText = document.querySelector(".CountNumText");
+const PepleNumText = document.querySelector(".PepleNumText");
+
+// function TextPrint(element) {
+//     TRtables.forEach(TRtable => {
+//         TRtable.addEventListener('click', () => {
+//             const clickTDS = TRtable.querySelectorAll(element);
+//             clickTDS.forEach(element => {
+//                 console.log(element.textContent);
+//                 details.classList.add("show");
+//             });
+//         });
+//     });
+// };
+
+TRtables.forEach(TRtable => {
+    TRtable.addEventListener('click', () => {
+        const clickTDS = TRtable.querySelectorAll(".NameTD");
+        clickTDS.forEach(e => {
+            WaiterName.textContent = e.textContent
+            details.classList.add("show");
+        });
+    });
+});
+TRtables.forEach(TRtable => {
+    TRtable.addEventListener('click', () => {
+        const clickTDS = TRtable.querySelectorAll(".MenTD");
+        clickTDS.forEach(e => {
+            PepleNumText.textContent = e.textContent
+        });
+    });
+});
+TRtables.forEach(TRtable => {
+    TRtable.addEventListener('click', () => {
+        const clickTDS = TRtable.querySelectorAll(".VisitTD");
+        clickTDS.forEach(e => {
+            CountNumText.textContent = e.textContent
+        });
+    });
+});
+TRtables.forEach(TRtable => {
+    TRtable.addEventListener('click', () => {
+        const clickTDS = TRtable.querySelectorAll(".PhoneTD");
+        clickTDS.forEach(e => {
+            NumText.textContent = e.textContent
+        });
+    });
+});
+
+
+X.addEventListener("click", () => {
+    details.classList.remove("show");
+});
