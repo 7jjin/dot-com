4a58cd4dc969215830b63f896a9395f92e77e9ac
7jjin <wlsgud6305@naver.com>
2023-04-01 20:29:07 +0900

    가게 페이지 이동 완료

--- JoJinHyeong/Store_info/createStore.js
+++ JoJinHyeong/Store_info/createStore.js
@@ -1,20 +1,33 @@
 //import {getSelectedValue} from '../mainPage/storeList.js';
 
-const selectedValue = sessionStorage.getItem("selectedValue");
-console.log(selectedValue)
+const value = sessionStorage.getItem("selectedValue");
+const storeName = document.querySelector(".storeName");
+const storeIntro = document.querySelector(".storeIntro");
+const addr = document.querySelector(".AddressInfo");
+const category1 = document.querySelector(".Menu1");
+const menuName1 = document.querySelector(".MenuName1");
+const menuName2 = document.querySelector(".MenuName2")
+const menuPrice1 = document.querySelector(".MenuPrice1");
+const menuPrice2 = document.querySelector(".MenuPrice2");
 
-fetch(" http://localhost:4000/store?adminNo=1")
+
+const url = `http://localhost:4000/store?adminNo=${value}`;
+fetch(url)
 .then((res)=>{
     return res.json()
 })
 .then((data)=>{
-    console.log(data);
+    renderPage(data);
 })
 .catch((err)=>console.log(err))
 
-// function Info(data){
-//     console.log(data)
-// }
-
-//http://127.0.0.1:5501/JoJinHyeong/Store_info/store.html
-
+function renderPage(data){
+    console.log(data[0]);
+    storeName.innerHTML=`${data[0].adminCafe}`
+    storeIntro.innerHTML=`${data[0].storeIntroduce}`
+    category1.innerHTML=`${data[0].categories[1].categoryName}`
+    menuName1.innerHTML=`${data[0].categories[1].menuList[0].menuName}`
+    menuPrice1.innerHTML=`${data[0].categories[1].menuList[0].menuPrice}`
+    menuName2.innerHTML=`${data[0].categories[1].menuList[1].menuName}`
+    menuPrice2.innerHTML=`${data[0].categories[1].menuList[1].menuPrice}`
+}
