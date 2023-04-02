5b64cc33f4923d19261acdaca4e8ce490b6b356f
7jjin <wlsgud6305@naver.com>
2023-04-02 22:29:52 +0900

    04/02

--- JoJinHyeong/mainPage/storeList.js
+++ JoJinHyeong/mainPage/storeList.js
@@ -14,44 +14,6 @@ fetch("http://localhost:4000/mainpage")
     console.log(error);
 });
 
-
-// function getSelectedValue(event){
-//     const value = event.target.parentNode.getAttribute("value");
-//     export {value} ;
-// }
-// let getSelectedValue =0;
-// //각각의 가게들을 만드는 함수
-//  function storeList(data){
-//     for(let i=0;i<data.length;i++){
-//         let adminCafe = data[i].adminCafe;
-//         let intro = data[i].storeIntroduce; 
-//         let addr = data[i].addressName;
-//         let stores = document.createElement("div");
-//         stores.setAttribute("class","store");
-//         stores.setAttribute("value",i+1);
-//         stores.innerHTML = `<div class="Sign" >
-//         <div class="Store_Sign">
-//           <div class="Store_Image"></div>
-//           <div class="Store_Name">
-//             <h4 class="Store_Title">"${adminCafe}"</h4>
-//             <p class="detail">"${intro}"</p>
-//             <div class="rating">
-//               <span class="Star">⭐</span>
-//               <span class="Star_Rating">4.5</span>
-//               <span class="Review_Rating">(412)</span>
-//             </div>
-//             <span class="tags">연어 및 각종 일식</span><br>
-//             <span class="address">"${addr}"</span>
-//           </div>
-//         </div>
-//       </div>`;
-//       stores.onclick = function(event){
-//         getSelectedValue = event.target.parentNode.getAttribute("value");
-//         console.log(getSelectedValue)
-//       };
-//       Store_Zone.appendChild(stores);
-//     }
-// }
 function storeList(data){
   for(let i=0;i<data.length;i++){
     let adminCafe = data[i].adminCafe;
@@ -60,7 +22,7 @@ function storeList(data){
     let stores = document.createElement("div");
     stores.setAttribute("class","store");
     stores.setAttribute("value",i+1);
-    stores.innerHTML = `<div class="Sign" onclick=location.href="/JoJinHyeong/Store_info/store.html">
+    stores.innerHTML = `<div class="Sign">
       <div class="Store_Sign">
         <div class="Store_Image"></div>
         <div class="Store_Name">
@@ -78,7 +40,7 @@ function storeList(data){
     </div>`;
       
     stores.onclick = function(event){
-      const selectedValue = event.target.parentNode.getAttribute("value");
+      const selectedValue = stores.getAttribute("value");
       sessionStorage.setItem("selectedValue", selectedValue);
       location.href="/JoJinHyeong/Store_info/store.html";
     };
@@ -86,5 +48,4 @@ function storeList(data){
     Store_Zone.appendChild(stores);
   }
 }
-
 
