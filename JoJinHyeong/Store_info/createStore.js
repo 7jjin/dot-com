d655b00b6c2b0f0247dbce5dddada5b958b25389
7jjin <wlsgud6305@naver.com>
2023-04-19 16:06:29 +0900

    가게 페이지 완성
    카테고리 개수에 따라 박스를 만들어서 추가하는 방식

--- JoJinHyeong/Store_info/createStore.js
+++ JoJinHyeong/Store_info/createStore.js
@@ -1,14 +1,15 @@
-
 const nameStore = document.querySelector(".name");
 const storeIntro = document.querySelector(".storeIntro");
 const addr = document.querySelector(".AddressInfo");
-const category1 = document.querySelector(".Menu1");
+const Menu = document.querySelector(".Menu");
 const menuName1 = document.querySelector(".MenuName1");
 const menuName2 = document.querySelector(".MenuName2")
 const menuPrice1 = document.querySelector(".MenuPrice1");
 const menuPrice2 = document.querySelector(".MenuPrice2");
 const call = document.querySelector(".CallInfo");
-
+const week = document.querySelector(".Week");
+const openTime = document.querySelector(".OpenTime");
+const closeTime = document.querySelector(".CloseTime");
 const uni = sessionStorage.getItem("selectedValue");
 const url = `http://localhost:4000/store?adminNo=${uni}`;
 fetch(url)
@@ -17,23 +18,69 @@ fetch(url)
 })
 .then((data)=>{
     renderPage(data);
+    openingDay(data);
 })
 .catch((err)=>console.log(err))
 
+
+
 function renderPage(data){
-    addr.innerHTML=`${data[0].addressName}`
-    call.innerHTML=`${data[0].storePhone}`
-    nameStore.innerHTML=`${data[0].adminCafe}`
-    storeIntro.innerHTML=`${data[0].storeIntroduce}`
-    category1.innerHTML=`${data[0].categories[1].categoryName}`
-    menuName1.innerHTML=`${data[0].categories[1].menuList[0].menuName}`
-    menuPrice1.innerHTML=`${data[0].categories[1].menuList[0].menuPrice}`
-    menuName2.innerHTML=`${data[0].categories[1].menuList[1].menuName}`
-    menuPrice2.innerHTML=`${data[0].categories[1].menuList[1].menuPrice}`
+    for(let i=0;i<data[0].categories.length;i++){
+        addr.innerHTML=`${data[0].addressName}`;
+        call.innerHTML=`${data[0].storePhone}`;
+        openTime.innerHTML = `${data[0].openingTime}`;
+        closeTime.innerHTML = `${data[0].closingTime}`;
+        nameStore.innerHTML=`${data[0].adminCafe}`;
+        storeIntro.innerHTML=`${data[0].storeIntroduce}`
+        
+        // 카테고리별로 박스 만들어서 appendChild로 추가
+        const menubox = document.createElement("div");
+        menubox.innerHTML = `<a class="MenuItem">
+        <h1 class="Menu${i+1}">${data[0].categories[i].categoryName}</h1>
+        <a class="SubItem">
+          <div class="MenuInfo">
+            <div class="MenuName">
+              <h4>
+                <strong class="MenuName1">${data[0].categories[i].menuList[0].menuName}</strong>
+              </h4>
+            </div>
+            <span class="MenuPrice1">${data[0].categories[i].menuList[0].menuPrice}</span>
+            <span>원</span>
+          </div>
+          <div class="MenuImg" style="background-image: URL(/img/Relief.jpg);"></div>
+        </a>
+        <a class="SubItem">
+          <div class="MenuInfo">
+            <div class="MenuName">
+              <h4>
+                <strong class="MenuName2">${data[0].categories[i].menuList[1].menuName}</strong>
+              </h4>
+            </div>
+            <span class="MenuPrice2">${data[0].categories[i].menuList[1].menuPrice}</span>
+            <span>원</span>
+          </div>
+          <div class="MenuImg" style="background-image: URL(/img/Tomahawk.jpg);"></div>
+        </a>
+      </a>
+      <hr class="line"></hr>`
+    Menu.appendChild(menubox);
+    }
 }
 
-// export function fetchData(){
-//     return fetch(url)
-//         .then(res=>res.json())
-//         .catch(err=>console.log(err));
-// }
+
+// 운영요일 구하는 함수
+function openingDay(data){
+    const day = String(data[0].weekday);
+    let daylist = "";
+    for(let i=0;i<day.length;i++){
+        console.log(typeof day[i])
+        if(day[i]==="0") daylist+="일 ";
+        else if(day[i]==="1") daylist+="월 ";
+        else if(day[i]==="2") daylist+="화 ";
+        else if(day[i]==="3") daylist+="수 ";
+        else if(day[i]==="4") daylist+="목 ";
+        else if(day[i]==="5") daylist+="금 ";
+        else if(day[i]==="6") daylist+="토 ";
+    }
+    week.innerHTML = daylist
+}
