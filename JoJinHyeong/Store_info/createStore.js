bc8acb0b700a97e932c9e6d280a4557f5251e5ed
7jjin <wlsgud6305@naver.com>
2023-04-29 22:40:03 +0900

    지역 필터 기본값으로 서울지역이 클린된 상태로 나온다.

--- JoJinHyeong/Store_info/createStore.js
+++ JoJinHyeong/Store_info/createStore.js
@@ -3,7 +3,7 @@ const storeIntro = document.querySelector(".storeIntro");
 const addr = document.querySelector(".AddressInfo");
 const Menu2 = document.querySelector(".Menu");
 const menuName1 = document.querySelector(".MenuName1");
-const menuName2 = document.querySelector(".MenuName2")
+const menuName2 = document.querySelector(".MenuName2");
 const menuPrice1 = document.querySelector(".MenuPrice1");
 const menuPrice2 = document.querySelector(".MenuPrice2");
 const call = document.querySelector(".CallInfo");
@@ -14,36 +14,33 @@ const closeTime = document.querySelector(".CloseTime");
 const uni = sessionStorage.getItem("selectedValue");
 const url = `http://localhost:4000/store?adminNo=${uni}`;
 fetch(url)
-    .then((res) => {
-        return res.json()
-    })
-    .then((data) => {
-        openingDay(data);
-        renderPage(data);
-
-    })
-    .catch((err) => console.log(err))
-
-
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    openingDay(data);
+    renderPage(data);
+  })
+  .catch((err) => console.log(err));
 
 function renderPage(data) {
-    for (let i = 0; i < data[0].categories.length; i++) {
-        addr.innerHTML = `${data[0].addressName}`;
-        call.innerHTML = `${data[0].storePhone}`;
-        openTime.innerHTML = `${data[0].openingTime}`;
-        closeTime.innerHTML = `${data[0].closingTime}`;
-        nameStore.innerHTML = `${data[0].adminCafe}`;
-        storeIntro.innerHTML = `${data[0].storeIntroduce}`
-        // 카테고리별로 박스 만들어서 appendChild로 추가
-        const menubox = document.createElement("div");
-        menubox.innerHTML = `<a class="MenuItem">
+  for (let i = 0; i < data[0].categories.length; i++) {
+    addr.innerHTML = `${data[0].addressName}`;
+    call.innerHTML = `${data[0].storePhone}`;
+    openTime.innerHTML = `${data[0].openingTime}`;
+    closeTime.innerHTML = `${data[0].closingTime}`;
+    nameStore.innerHTML = `${data[0].adminCafe}`;
+    storeIntro.innerHTML = `${data[0].storeIntroduce}`;
+    // 카테고리별로 박스 만들어서 appendChild로 추가
+    const menubox = document.createElement("div");
+    menubox.innerHTML = `<a class="MenuItem">
         <h1 class="Menu${i + 1}">${data[0].categories[i].categoryName}</h1>
-        </a>`
-        Menu2.appendChild(menubox);
-        console.log(data[0].categories[i].menuList)
-        for (let j = 0; j < data[0].categories[i].menuList.length; j++) {
-            const menulist = document.createElement("div");
-            menulist.innerHTML = `<a class="SubItem">
+        </a>`;
+    Menu2.appendChild(menubox);
+    console.log(data[0].categories[i].menuList);
+    for (let j = 0; j < data[0].categories[i].menuList.length; j++) {
+      const menulist = document.createElement("div");
+      menulist.innerHTML = `<a class="SubItem">
             <div class="MenuInfo">
               <div class="MenuName">
                 <h4>
@@ -55,24 +52,35 @@ function renderPage(data) {
             </div>
             <div class="MenuImg" style="background-image: URL(/img/Relief.jpg);"></div>
             </a>`;
-            Menu2.appendChild(menulist);
+      Menu2.appendChild(menulist);
 
-            PhotoTab.addEventListener('click', function () {
-                Move1(window.pageYOffset + document.querySelector('.storePhoto').getBoundingClientRect().top);
-            });
-            ReviewTab.addEventListener('click', function () {
-                Move1(window.pageYOffset + document.querySelector('.storeReview').getBoundingClientRect().top);
-            });
+      PhotoTab.addEventListener("click", function () {
+        Move1(
+          window.pageYOffset +
+            document.querySelector(".storePhoto").getBoundingClientRect().top
+        );
+      });
+      ReviewTab.addEventListener("click", function () {
+        Move1(
+          window.pageYOffset +
+            document.querySelector(".storeReview").getBoundingClientRect().top
+        );
+      });
 
-    // ------ 메뉴추가
-            const menu3 = document.querySelectorAll(".SubItem");
+      // ------ 메뉴추가
+      const menu3 = document.querySelectorAll(".SubItem");
 
-            menu3.forEach((item) => {
-                item.addEventListener("click", function (e) {
-                  const line = document.querySelector(".bottomline");
-                  const menuBoxOuter = document.querySelector(".menuBoxOuter");     // 총합DIV을 맨 아래 넣기 위해 각각의 메뉴태그들을 감싸는 부모 태그를 만들었다.
-                  if (cart.every((menu3) => menu3.name !== item.childNodes[1].childNodes[1].innerText)) {
-                    menuBoxOuter.innerHTML += `
+      menu3.forEach((item) => {
+        item.addEventListener("click", function (e) {
+          const line = document.querySelector(".bottomline");
+          const menuBoxOuter = document.querySelector(".menuBoxOuter"); // 총합DIV을 맨 아래 넣기 위해 각각의 메뉴태그들을 감싸는 부모 태그를 만들었다.
+          if (
+            cart.every(
+              (menu3) =>
+                menu3.name !== item.childNodes[1].childNodes[1].innerText
+            )
+          ) {
+            menuBoxOuter.innerHTML += `
                 <div class="menuBox">
                       <div class="menuName">${item.childNodes[1].childNodes[1].innerText}</div>
                       <div class="menuCost">
@@ -87,57 +95,55 @@ function renderPage(data) {
                           <button class="plus" onclick="plus(this)">+</button>
                         </div>
                 </div>`;
-                    const product = {
-                      name: `${item.childNodes[1].childNodes[1].innerText}`,
-                      quantity: 1,
-                      price: Number(item.childNodes[1].childNodes[3].innerText)
-                    };
-                    // 새로 추가될때마다 총합 태그는 가장 아래 위치됨
-                    cart.push(product);
-          
-                    line.style.visibility = "visible";
-                    const costhap = document.querySelector(".costHap");
-                    const count = document.querySelector(".count");
-                    costhap.style.display = "flex";       // 합계 DIV 보이게 하기
-                    count.style.display = "block";
-                    costhap.innerHTML = `
+            const product = {
+              name: `${item.childNodes[1].childNodes[1].innerText}`,
+              quantity: 1,
+              price: Number(item.childNodes[1].childNodes[3].innerText),
+            };
+            // 새로 추가될때마다 총합 태그는 가장 아래 위치됨
+            cart.push(product);
+
+            line.style.visibility = "visible";
+            const costhap = document.querySelector(".costHap");
+            const count = document.querySelector(".count");
+            costhap.style.display = "flex"; // 합계 DIV 보이게 하기
+            count.style.display = "block";
+            costhap.innerHTML = `
                 <div class="hapDiv">
                   <div class="hapname">합계: </div>
                   <div class="hap"></div>
                   <div>원</div>
                 </div>
-                `
-                    count.innerHTML = `
-                <button>주문하기</button>`
-                    hap();
-                  }
-                });
-              })
-        }
-        const bar = document.createElement("hr");
-        bar.className = "line";
-        Menu2.append(bar)
+                `;
+            count.innerHTML = `
+                <button>주문하기</button>`;
+            hap();
+          }
+        });
+      });
     }
+    const bar = document.createElement("hr");
+    bar.className = "line";
+    Menu2.append(bar);
+  }
 }
 
 function Move1(element) {
-    window.scroll({ top: element - 95, behavior: 'smooth' });
+  window.scroll({ top: element - 95, behavior: "smooth" });
 }
-
 
 // 운영요일 구하는 함수
 function openingDay(data) {
-    const day = String(data[0].weekday);
-    let daylist = "";
-    for (let i = 0; i < day.length; i++) {
-        if (day[i] === "0") daylist += "일 ";
-        else if (day[i] === "1") daylist += "월 ";
-        else if (day[i] === "2") daylist += "화 ";
-        else if (day[i] === "3") daylist += "수 ";
-        else if (day[i] === "4") daylist += "목 ";
-        else if (day[i] === "5") daylist += "금 ";
-        else if (day[i] === "6") daylist += "토 ";
-    }
-    week.innerHTML = daylist
+  const day = String(data[0].weekday);
+  let daylist = "";
+  for (let i = 0; i < day.length; i++) {
+    if (day[i] === "0") daylist += "일 ";
+    else if (day[i] === "1") daylist += "월 ";
+    else if (day[i] === "2") daylist += "화 ";
+    else if (day[i] === "3") daylist += "수 ";
+    else if (day[i] === "4") daylist += "목 ";
+    else if (day[i] === "5") daylist += "금 ";
+    else if (day[i] === "6") daylist += "토 ";
+  }
+  week.innerHTML = daylist;
 }
-
--- JoJinHyeong/mainPage/RegionFilter.js
+++ JoJinHyeong/mainPage/RegionFilter.js
@@ -9,6 +9,19 @@ dooicon.addEventListener("click", function () {
   }
 });
 
+// 기본 지역 선택은 서울로 초기화
+const seoullist = outerRegion.querySelector(".Region_si ul[data-region=seoul]");
+const allLiElements = outerRegion.querySelectorAll(".Region_si li");
+allLiElements.forEach(function (li) {
+  if (li.parentElement === seoullist) {
+    // 서울지역만 나오게 하기
+    li.style.display = "block";
+  } else {
+    li.style.display = "none"; // 나머지는 안보이게 하기
+  }
+});
+
+//각각의 지역을 클릭했을 때 해당 지역 도시 나오게 하기
 regionLinks.forEach(function (link) {
   link.addEventListener("click", function (event) {
     const region = this.dataset.region;
@@ -16,12 +29,10 @@ regionLinks.forEach(function (link) {
       `.Region_si ul[data-region="${region}"]`
     ); // data-region 속성 값이 해당 지역 이름으로 시작하는 ul 요소들
 
-    // 모든 지역에 대한 li 요소를 숨김
-    const allLiElements = outerRegion.querySelectorAll(".Region_si li");
+    // 클릭됬을때 나머지는 지우고 해당 지역의 li만 보이게 하기
     allLiElements.forEach(function (li) {
       li.style.display = "none";
     });
-    // 해당 지역에 대한 li 요소를 보임
     regionLists.forEach(function (list) {
       const liElements = list.querySelectorAll("li");
       liElements.forEach(function (li) {
@@ -29,4 +40,17 @@ regionLinks.forEach(function (link) {
       });
     });
   });
+});
+
+function handleClick(event) {
+  regionLinks.forEach((e) => {
+    e.classList.remove("doo_color");
+  });
+  event.target.closest("li").classList.add("doo_color");
+}
+regionLinks.forEach((e) => {
+  if (e.dataset.region === "seoul") {
+    e.classList.add("doo_color");
+  }
+  e.addEventListener("click", handleClick);
 });
