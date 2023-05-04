9a46a2623b7388015a95251bc83569d30e31a04f
7jjin <wlsgud6305@naver.com>
2023-05-04 11:51:31 +0900

    소셜 로그인 password창

---
+++ JoJinHyeong/mainPage/inputPassword.js
@@ -0,0 +1,32 @@
+const password = document.querySelector(".phone_input input");
+const submit = document.querySelector(".phone_submit");
+
+function Inputpassword(target) {
+  target.value = target.value
+    .replace(/[^0-9]/g, "")
+    .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
+    .replace(/(\-{1,2})$/g, "");
+}
+
+function submitpassword() {
+  const len = password.value.length;
+  if (len === 13) {
+    fetch(" http://localhost:4000/password", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        password: password.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        // location.href = "/JoJinHyeong/mainPage/main.html";
+        document.querySelector(".socialLogin_phone").style.display = "none";
+      })
+      .catch((err) => console.log(err));
+  } else {
+    alert("전화번호를 다시 입력해주세요!");
+  }
+}
