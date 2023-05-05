fe6d043b8733040addbb868bafdf9ed6ef22298e
7jjin <wlsgud6305@naver.com>
2023-05-05 11:12:16 +0900

    로그인에 따른 전화번호 나오게하기

--- JoJinHyeong/mainPage/inputPassword.js
+++ JoJinHyeong/mainPage/inputPassword.js
@@ -7,9 +7,9 @@ function Inputpassword(target) {
     .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
     .replace(/(\-{1,2})$/g, "");
 }
-
 function submitpassword() {
   const len = password.value.length;
+  const phone = password.value;
   if (len === 13) {
     fetch(" http://localhost:4000/password", {
       method: "POST",
@@ -30,3 +30,12 @@ function submitpassword() {
     alert("전화번호를 다시 입력해주세요!");
   }
 }
+
+fetch("http://localhost:4000/login")
+  .then((res) => res.json())
+  .then((data) => {
+    if (data[0].provider === "kakao") {
+      document.querySelector(".socialLogin_phone").style.display = "block";
+    }
+  })
+  .catch((err) => console.log(err));
