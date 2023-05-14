90e531a950ed712649ad06755abe923320b2e4f3
7jjin <wlsgud6305@naver.com>
2023-05-14 21:24:00 +0900

    Update Owner_Wait_DB.js

--- JoSeungJun/Owner_Wait/Owner_Wait_DB.js
+++ JoSeungJun/Owner_Wait/Owner_Wait_DB.js
@@ -17,18 +17,18 @@ fetch("http://localhost:4000/waiting")
   });
 
 function waitingList(data) {
-    for (let i = 0; i < data.length; i++) {
-        let waitingName = data[i].name;
-        let waitingNum = data[i].phone_number;
-        let waitingSize = data[i].party_size;
-        let waitingQue = data[i].queueNumber;
-        let blacklist = data[i].black;
-        let waitings = document.createElement("tr");
-        if (blacklist === 1) {
-            waitings.className = "TRtable black";
-          } else {
-            waitings.className = "TRtable";
-          }
+  for (let i = 0; i < data.length; i++) {
+    let waitingName = data[i].name;
+    let waitingNum = data[i].phone_number;
+    let waitingSize = data[i].party_size;
+    let waitingQue = data[i].queueNumber;
+    let blacklist = data[i].black;
+    let waitings = document.createElement("tr");
+    if (data[i].black === 1) {
+      waitings.className = "TRtable black";
+    } else {
+      waitings.className = "TRtable";
+    }
 
     let WaiterName = document.querySelector(".WaiterName");
     let NumText = document.querySelector(".NumText");
@@ -45,35 +45,39 @@ function waitingList(data) {
             </tr>`;
     wait_Zone.append(waitings);
 
-        let waitingsChildren = waitings.children;
-        if (blacklist === 1) {
-            for (let i = 0; i < waitingsChildren.length; i++) {
-              waitingsChildren[i].style.color = "red";
-          }
-        }
-          
-        let Selects = document.querySelectorAll('.TRtable td');
-        Selects.forEach(function (Select) {
-            Select.addEventListener("click", function () {
-                WaiterName.textContent = Select.parentNode.querySelector('.NameTD').textContent;
-                NumText.textContent = Select.parentNode.querySelector('.PhoneTD').textContent;
-                CountNum.textContent = Select.parentNode.querySelector('.VisitTD').textContent;
-                PeopleText.textContent = Select.parentNode.querySelector('.MenTD').textContent;
-
-                if (Select.parentNode.classList.contains('black')) {
-                    lockBtn.style.display = "block";
-                    unlockBtn.style.display = "none";
-                    BlackPeople.style.display = "block"
-                  } else {
-                    lockBtn.style.display = "none";
-                    unlockBtn.style.display = "block";
-                    BlackPeople.style.display = "none";
-                  }
-
-                showdetails();     
-            });
-        });
+    let waitingsChildren = waitings.children;
+    if (blacklist === 1) {
+      for (let i = 0; i < waitingsChildren.length; i++) {
+        waitingsChildren[i].style.color = "red";
+      }
     }
+
+    let Selects = document.querySelectorAll(".TRtable td");
+    Selects.forEach(function (Select) {
+      Select.addEventListener("click", function () {
+        WaiterName.textContent =
+          Select.parentNode.querySelector(".NameTD").textContent;
+        NumText.textContent =
+          Select.parentNode.querySelector(".PhoneTD").textContent;
+        CountNum.textContent =
+          Select.parentNode.querySelector(".VisitTD").textContent;
+        PeopleText.textContent =
+          Select.parentNode.querySelector(".MenTD").textContent;
+
+        if (Select.parentNode.classList.contains("black")) {
+          lockBtn.style.display = "block";
+          unlockBtn.style.display = "none";
+          BlackPeople.style.display = "block";
+        } else {
+          lockBtn.style.display = "none";
+          unlockBtn.style.display = "block";
+          BlackPeople.style.display = "none";
+        }
+
+        showdetails();
+      });
+    });
+  }
 }
 
 function showdetails() {
