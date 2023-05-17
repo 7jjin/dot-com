d656034bde248d8db7420245cc42a26bae21bb26
7jjin <wlsgud6305@naver.com>
2023-05-17 11:21:30 +0900

    지역 필터를 누르고 메뉴필터 적용 성공

--- JoJinHyeong/mainPage/RegionFilter.js
+++ JoJinHyeong/mainPage/RegionFilter.js
@@ -146,6 +146,7 @@ function check() {
             Store_Zone.appendChild(Closestores);
           }
         });
+        menuClick_Mul(data, array);
       } else if (array.length === 1) {
         // 지역 선택을 한 곳만 했을 경우
         checkedRegion.innerHTML = array[0];
@@ -214,6 +215,7 @@ function check() {
             Store_Zone.appendChild(Closestores);
           }
         });
+        menuClick_sol(data, array);
       } else {
         // 지역 선택을 안했을 경우
         checkedRegion.innerHTML = "예) 경기도 수원시";
@@ -283,6 +285,7 @@ function check() {
           Store_Zone.appendChild(Openstores);
           Store_Zone.appendChild(Closestores);
         }
+        menuClick(data);
       }
       outerRegion.style.display = "none";
     })
@@ -291,6 +294,259 @@ function check() {
     });
 }
 document.querySelector(".findRegion").addEventListener("click", check);
+
+function menuClick_Mul(data, array) {
+  const sortedMenu = document.querySelectorAll("#sortedMenu");
+  sortedMenu.forEach((el) => {
+    el.addEventListener("click", function () {
+      Store_Zone.innerHTML = "";
+      let menus = el.parentElement.lastElementChild.innerHTML; //한식
+      data.forEach((item) => {
+        if (
+          item.foodType === menus &&
+          array.includes(item.addressName.split(" ")[1])
+        ) {
+          let adminCafe = item.adminCafe;
+          let intro = item.storeIntroduce;
+          let addr = item.addressName;
+          let adminNo = item.adminNo;
+          let open = item.open;
+          let waitingNum = item.waitingNum;
+          let foodType = item.foodType;
+          let Openstores = document.createElement("div");
+          let Closestores = document.createElement("div");
+          Openstores.setAttribute("value", foodType);
+          Closestores.setAttribute("value", foodType);
+
+          Openstores.setAttribute("class", "store");
+          Closestores.setAttribute("class", "store");
+
+          //stores.setAttribute("value",i+1);
+          if (open === true) {
+            Openstores.innerHTML = `<div class="Sign">
+              <div class="Store_Sign">
+                <div class="Store_Image"></div>
+                <div class="Store_Name">
+                  <h4 class="Store_Title">"${adminCafe}"</h4>
+                  <p class="detail">"${intro}"</p>
+                  <div class="rating">
+                    <span class="Star">⭐</span>
+                    <span class="Star_Rating">4.5</span>
+                    <span class="Review_Rating">(412)</span>
+                  </div>
+                  <span class="tags">연어 및 각종 일식</span><br>
+                  <span class="address">"${addr}"</span>
+                </div>
+              </div>
+              <div class="WaitingBox">
+              <p class="WaitingNum">${waitingNum}</p>
+              <p>명</p>
+            </div>
+            </div>`;
+          } else if (open === false) {
+            Closestores.innerHTML = `<div class="CloseSign">
+              <div class="Store_Sign">
+                <div class="Store_Image"></div>
+                <div class="Store_Name">
+                  <h4 class="Store_Title">"${adminCafe}"</h4>
+                  <p class="detail">"${intro}"</p>
+                  <div class="rating">
+                    <span class="Star">⭐</span>
+                    <span class="Star_Rating">4.5</span>
+                    <span class="Review_Rating">(412)</span>
+                  </div>
+                  <span class="tags">연어 및 각종 일식</span><br>
+                  <span class="address">"${addr}"</span>
+                </div>
+              </div>
+              <span class="bad">
+              <p>예약</p>
+              <p>불가</p>
+              </span>
+            </div>`;
+          }
+          Openstores.onclick = function (event) {
+            sessionStorage.setItem("selectedValue", adminNo); //sessionStorage에 가게고유의 adminNo값 저장
+            window.location.href = "/JoJinHyeong/Store_info/store.html";
+          };
+          Closestores.onclick = function (event) {
+            sessionStorage.setItem("selectedValue", adminNo); //sessionStorage에 가게고유의 adminNo값 저장
+            window.location.href = "/JoJinHyeong/Store_info/store.html";
+          };
+          Store_Zone.appendChild(Openstores);
+          Store_Zone.appendChild(Closestores);
+        }
+      });
+    });
+  });
+}
+function menuClick_sol(data, array) {
+  const sortedMenu = document.querySelectorAll("#sortedMenu");
+  sortedMenu.forEach((el) => {
+    el.addEventListener("click", function () {
+      Store_Zone.innerHTML = "";
+      let menus = el.parentElement.lastElementChild.innerHTML; //한식
+      data.forEach((item) => {
+        if (
+          item.foodType === menus &&
+          item.addressName.split(" ")[1] === array[0]
+        ) {
+          let adminCafe = item.adminCafe;
+          let intro = item.storeIntroduce;
+          let addr = item.addressName;
+          let adminNo = item.adminNo;
+          let open = item.open;
+          let waitingNum = item.waitingNum;
+          let foodType = item.foodType;
+          let Openstores = document.createElement("div");
+          let Closestores = document.createElement("div");
+          Openstores.setAttribute("value", foodType);
+          Closestores.setAttribute("value", foodType);
+
+          Openstores.setAttribute("class", "store");
+          Closestores.setAttribute("class", "store");
+
+          //stores.setAttribute("value",i+1);
+          if (open === true) {
+            Openstores.innerHTML = `<div class="Sign">
+              <div class="Store_Sign">
+                <div class="Store_Image"></div>
+                <div class="Store_Name">
+                  <h4 class="Store_Title">"${adminCafe}"</h4>
+                  <p class="detail">"${intro}"</p>
+                  <div class="rating">
+                    <span class="Star">⭐</span>
+                    <span class="Star_Rating">4.5</span>
+                    <span class="Review_Rating">(412)</span>
+                  </div>
+                  <span class="tags">연어 및 각종 일식</span><br>
+                  <span class="address">"${addr}"</span>
+                </div>
+              </div>
+              <div class="WaitingBox">
+              <p class="WaitingNum">${waitingNum}</p>
+              <p>명</p>
+            </div>
+            </div>`;
+          } else if (open === false) {
+            Closestores.innerHTML = `<div class="CloseSign">
+              <div class="Store_Sign">
+                <div class="Store_Image"></div>
+                <div class="Store_Name">
+                  <h4 class="Store_Title">"${adminCafe}"</h4>
+                  <p class="detail">"${intro}"</p>
+                  <div class="rating">
+                    <span class="Star">⭐</span>
+                    <span class="Star_Rating">4.5</span>
+                    <span class="Review_Rating">(412)</span>
+                  </div>
+                  <span class="tags">연어 및 각종 일식</span><br>
+                  <span class="address">"${addr}"</span>
+                </div>
+              </div>
+              <span class="bad">
+              <p>예약</p>
+              <p>불가</p>
+              </span>
+            </div>`;
+          }
+          Openstores.onclick = function (event) {
+            sessionStorage.setItem("selectedValue", adminNo); //sessionStorage에 가게고유의 adminNo값 저장
+            window.location.href = "/JoJinHyeong/Store_info/store.html";
+          };
+          Closestores.onclick = function (event) {
+            sessionStorage.setItem("selectedValue", adminNo); //sessionStorage에 가게고유의 adminNo값 저장
+            window.location.href = "/JoJinHyeong/Store_info/store.html";
+          };
+          Store_Zone.appendChild(Openstores);
+          Store_Zone.appendChild(Closestores);
+        }
+      });
+    });
+  });
+}
+function menuClick(data) {
+  const sortedMenu = document.querySelectorAll("#sortedMenu");
+  sortedMenu.forEach((el) => {
+    el.addEventListener("click", function () {
+      Store_Zone.innerHTML = "";
+      let menus = el.parentElement.lastElementChild.innerHTML; //한식
+      data.forEach((item) => {
+        if (item.foodType === menus) {
+          let adminCafe = item.adminCafe;
+          let intro = item.storeIntroduce;
+          let addr = item.addressName;
+          let adminNo = item.adminNo;
+          let open = item.open;
+          let waitingNum = item.waitingNum;
+          let foodType = item.foodType;
+          let Openstores = document.createElement("div");
+          let Closestores = document.createElement("div");
+          Openstores.setAttribute("value", foodType);
+          Closestores.setAttribute("value", foodType);
+
+          Openstores.setAttribute("class", "store");
+          Closestores.setAttribute("class", "store");
+
+          //stores.setAttribute("value",i+1);
+          if (open === true) {
+            Openstores.innerHTML = `<div class="Sign">
+              <div class="Store_Sign">
+                <div class="Store_Image"></div>
+                <div class="Store_Name">
+                  <h4 class="Store_Title">"${adminCafe}"</h4>
+                  <p class="detail">"${intro}"</p>
+                  <div class="rating">
+                    <span class="Star">⭐</span>
+                    <span class="Star_Rating">4.5</span>
+                    <span class="Review_Rating">(412)</span>
+                  </div>
+                  <span class="tags">연어 및 각종 일식</span><br>
+                  <span class="address">"${addr}"</span>
+                </div>
+              </div>
+              <div class="WaitingBox">
+              <p class="WaitingNum">${waitingNum}</p>
+              <p>명</p>
+            </div>
+            </div>`;
+          } else if (open === false) {
+            Closestores.innerHTML = `<div class="CloseSign">
+              <div class="Store_Sign">
+                <div class="Store_Image"></div>
+                <div class="Store_Name">
+                  <h4 class="Store_Title">"${adminCafe}"</h4>
+                  <p class="detail">"${intro}"</p>
+                  <div class="rating">
+                    <span class="Star">⭐</span>
+                    <span class="Star_Rating">4.5</span>
+                    <span class="Review_Rating">(412)</span>
+                  </div>
+                  <span class="tags">연어 및 각종 일식</span><br>
+                  <span class="address">"${addr}"</span>
+                </div>
+              </div>
+              <span class="bad">
+              <p>예약</p>
+              <p>불가</p>
+              </span>
+            </div>`;
+          }
+          Openstores.onclick = function (event) {
+            sessionStorage.setItem("selectedValue", adminNo); //sessionStorage에 가게고유의 adminNo값 저장
+            window.location.href = "/JoJinHyeong/Store_info/store.html";
+          };
+          Closestores.onclick = function (event) {
+            sessionStorage.setItem("selectedValue", adminNo); //sessionStorage에 가게고유의 adminNo값 저장
+            window.location.href = "/JoJinHyeong/Store_info/store.html";
+          };
+          Store_Zone.appendChild(Openstores);
+          Store_Zone.appendChild(Closestores);
+        }
+      });
+    });
+  });
+}
 
 //초기화 버튼 기능
 const resetbutton = document.querySelector(".resetbutton");
